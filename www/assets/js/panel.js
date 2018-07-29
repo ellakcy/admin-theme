918f2b6a8d59b0f20953d72afe961741562fb804
Dimitrios Desyllas (pc_magas) <you@example.com>
2018-07-29 16:14:53 +0300

    on small screen show dropdown replacements

--- www/assets/js/panel.js
+++ www/assets/js/panel.js
@@ -6,10 +6,29 @@ var isDesktop = function(){
 }
 
 /**
-* In many cases we want to retrieve an element that
+* In many cases we want to retrieve an element that has an id defined in an attribute.
+* @param {Element} element The element to retrieve the id.
+* @param {String} attribute the attribute containing the id.
 */
-var getElementFromIdProvidedInDataAttribute = function(parentElement, dataAttribute){
+var getElementFromIdProvidedInDataAttribute = function(element, attribute){
+  var href=$(element).attr(attribute);
+  href=document.getElementById(href);
+  return href;
+}
 
+/**
+* Convert a value into a boolean
+* @param {Mixed} value The value to check convert into boolean
+* @return {Boolean}
+*/
+var boolVal=function(value){
+  var falseValues=['false',0,undefined,'0','no','null',null];
+
+  if (typeof value === 'string' || value instanceof String){
+      value=value.toLowerCase();
+  }
+
+  return $.inArray(value, falseValues) == -1
 }
 
 $(function(){
@@ -54,10 +73,20 @@ $(function(){
 
   $("#sidebarMenus div button").click(function(e){
     e.preventDefault();
-    console.log("Item Clicked");
-    var href=$(this).attr("data-sidebar-show");
-    href=document.getElementById(href);
-    console.log(href);
+    var href=getElementFromIdProvidedInDataAttribute(this,"data-sidebar-show");
+
+    if(href && !boolVal($(href).attr('data-sidebar-sm-display'))){
+      $('.sidedar-sm[data-sidebar-sm-display="true"]').toggle("slide",function(){
+        $(this).attr('data-sidebar-sm-display',false);
+      });
+
+      $(href).toggle("slide",function(){
+        $(this).attr('data-sidebar-sm-display',true);
+      })
+    } else {
+      console.log("Not shown");
+    }
+
   })
 
 });
