8457129e11acd9be6248dfd3d6456e56dfed79b4
pcmagas <kattanophoros@gmail.com>
2018-08-23 00:09:18 +0300

    Using flex and a better approach using boootstrap's utilities

--- www/assets/js/panel.js
+++ www/assets/js/panel.js
@@ -37,34 +37,17 @@ $(function(){
     e.preventDefault();
     if($(window).width() > 700){
       $('#sidebar').toggle("slide",function(){
-        if($('#sidebar').is(":visible")){
-          $('.content').removeClass('full_width');
-        } else {
-          $('.content').addClass('full_width');
-        }
+        $('#sidebar').toggleClass('d-none d-md-flex flex-column');
       });
     } else {
-      $('#sidebar').toggle("blind");
+      $('#sidebar').toggle("blind",function(){
+        $('#sidebar').toggleClass('d-none d-md-flex flex-column');
+      });
     }
     $('.sidebar-reveal').blur();
   });
 
   $(window).on('resize orientationChange', function(event) {
-    if(isDesktop()){
-        $('#sidebar').show();
-        $('.dropdown-menu header-dropdown').addClass('dropdown-menu-right');
-
-        //Show Main sidebar content
-        $('#sideber-main-nav').show();
-        $('.sidedar-sm').attr('data-sidebar-sm-display',false);
-        $('#sideber-main-nav').attr('data-sidebar-sm-display',true);
-
-    } else {
-        $('#sidebar').hide();
-        $('.dropdown-menu header-dropdown').removeClass('dropdown-menu-right');
-    }
-    $('.sidebar-reveal').blur();
-    $('.content').removeClass('full_width');
   });
 
   $('.menu-open').click(function(e){
