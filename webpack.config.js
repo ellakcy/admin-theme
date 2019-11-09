06e53e781469151e0a19592582a659ad821c3ce4
pc-magas <katanophoros@gmail.com>
2019-11-09 17:20:03 +0200

    Rebuild using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -39,13 +39,20 @@ module.exports = {
         },
         {
             test: /\.s[ac]ss$/i,
-            use: [
+            use: [ 
+              {
+                loader: 'file-loader',
+                options: {
+                    name:  __dirname+'/assets/css/[name].css',
+                }
+              },
               // Creates `style` nodes from JS strings
               'style-loader',
               // Translates CSS into CommonJS
               'css-loader',
               // Compiles Sass to CSS
               'sass-loader',
+
             ],
         }, 
       ],
